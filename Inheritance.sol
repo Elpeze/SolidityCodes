696ac9958453e2e16a582ad4f23bafa6c08c7957
izzet <74833625+Elpeze@users.noreply.github.com>
2023-01-18 00:43:03 +0300

    Update Inheritance.sol

--- Inheritance.sol
+++ Inheritance.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.0;
 
 contract Human {
     function sayHello() public pure virtual returns(string memory) {
-        return "itublockchain.com adresi uzerinden kulube uye olabilirsiniz :)";
+        return "Hello contract";
     }
 }
 
 contract SuperHuman is Human {
     function sayHello() public pure override returns(string memory) {
-        return "Selamlar itu blockchain uyesi, nasilsin ? :)";
+        return "Hello member :)";
     }
 
     function welcomeMsg(bool isMember) public pure returns(string memory) {
